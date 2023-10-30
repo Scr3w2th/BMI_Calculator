dc2d95e4848b255579aec4a348fd0c8ffdc5b4d4
dlee74@msudenver.edu <92610685+Scr3w2th@users.noreply.github.com>
2023-10-30 12:56:11 -0600

    Finished GUI by adding vars; bmi_Val, stat, status, calculate, clear.\n Added .grid to said vars.\n At this juncture GUI runs, this code does not include 'command=calc' or 'command=clear' parameters on their respective vars. I will add these right after this push.

--- BMI.py
+++ BMI.py
@@ -15,3 +15,29 @@ w_label = Label(root, text="height", fg="red", bg="black", font=("Calibri 14 bol
 weigh = Entry(root, textvariable=weight)
 w_label.grid(row=4)
 weigh.grid(row=4, column=1)
+
+bmi_Val = DoubleVar()
+stat = StringVar()
+bmi = Label(root, text="BMI",fg="blue", bg="black", font=("Calibri 14 bold"), padx=3, pady=9, justify=LEFT)
+status = Label(root, text="Status", fg="blue", bg="black", font=("Calibri 14 bold"), padx=3, pady=9)
+status_msg = Label(root, textvariable=stat, fg="white", bg="black", font=("Calibri 14 bold"), padx=3, pady=9 )
+BMI_Total = Label(root, textvariable=bmi_Val, fg="white", bg="black", font=("Calibri 14 bold"), padx=3, pady=9)
+
+bmi.grid(row=6)
+status.grid(row=7)
+BMI_Total.grid(row=6, column=1)
+status_msg.grid(row=7, column=1)
+
+calculate = Button(root, text="Calculate",fg="black", bg="white", font=("Calibri 12 bold"))
+clear = Button(root, text="Reset", fg="black", bg="white", font=("Calibri 14 bold"))
+
+calculate.grid(row=8)
+clear.grid(row=8, column=3)
+root.mainloop()
+
+
+
+
+
+
+
