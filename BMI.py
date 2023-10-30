20b9838e42189332c2aca0a06b8f9b5120304e82
dlee74@msudenver.edu <92610685+Scr3w2th@users.noreply.github.com>
2023-10-30 12:19:30 -0600

    Added root, height, and weight variables for GUI.

--- BMI.py
+++ BMI.py
@@ -1,2 +1,17 @@
 from tkinter import *
 
+root = Tk()
+root.title("BMI CALCULATOR")
+root.configure(width=153, height=108)
+root.configure(bg="black")
+
+height = DoubleVar()
+h_label = Label(root, text="height", fg="red", bg="black", font=("Calibri 14 bold"), pady=6, padx=9)
+heigh = Entry(root, textvariable=height)
+h_label.grid(row=2, column=1, columnspan=2, padx=6)
+
+weight = DoubleVar()
+w_label = Label(root, text="height", fg="red", bg="black", font=("Calibri 14 bold"), pady=9, padx=3)
+weigh = Entry(root, textvariable=weight)
+w_label.grid(row=4)
+weigh.grid(row=4, column=1)
